4f61bf08e6c93480a0026d4e7ed63fd586f6ced4
0xlegendkiller <60841283+0xLegendKiller@users.noreply.github.com>
2022-08-18 23:36:06 +0530

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-echo "
-"
+cat <<'BANNER'
+
+   ___       _                               _ _  ___ _ _           
+  / _ \     | |                             | | |/ (_) | |          
+ | | | |_  _| |     ___  __ _  ___ _ __   __| | ' / _| | | ___ _ __ 
+ | | | \ \/ / |    / _ \/ _` |/ _ \ '_ \ / _` |  < | | | |/ _ \ '__|
+ | |_| |>  <| |___|  __/ (_| |  __/ | | | (_| | . \| | | |  __/ |   
+  \___//_/\_\______\___|\__, |\___|_| |_|\__,_|_|\_\_|_|_|\___|_|   
+                         __/ |                                      
+                        |___/    ~ https://twitter.com/0xLegendKiller
+ (A simple recon script inspired by Tomnomnom's bug bounty methodology)
+BANNER
 
 echo "####### Using wildacards ###########"
 cat wildcards.txt | assetfinder -subs-only | anew domains
