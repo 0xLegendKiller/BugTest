6cfffbda9f5c2dedb542adfdb34c2d8b313ff884
0xlegendkiller <60841283+0xLegendKiller@users.noreply.github.com>
2023-08-16 02:02:39 +0530

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -1,22 +1,15 @@
+
 #!/bin/bash
-cat <<'BANNER'
-
-   ___       _                               _ _  ___ _ _           
-  / _ \     | |                             | | |/ (_) | |          
- | | | |_  _| |     ___  __ _  ___ _ __   __| | ' / _| | | ___ _ __ 
- | | | \ \/ / |    / _ \/ _` |/ _ \ '_ \ / _` |  < | | | |/ _ \ '__|
- | |_| |>  <| |___|  __/ (_| |  __/ | | | (_| | . \| | | |  __/ |   
-  \___//_/\_\______\___|\__, |\___|_| |_|\__,_|_|\_\_|_|_|\___|_|   
-                         __/ |                                      
-                        |___/    ~ https://twitter.com/0xLegendKiller
- (A simple recon script inspired by Tomnomnom's bug bounty methodology)
-BANNER
-
-echo "####### Using wildcards ###########"
-cat wildcards.txt | assetfinder -subs-only | anew domains
-echo "####### HTTProbe ###########"
-cat domains | httprobe -c 80 --prefer-https | anew hosts
-echo "####### Using fff's fuffing ###########"
-echo "####### Keep Calm ! ###########"
-cat hosts | fff -d 1 -S -o roots
-echo "####### All done ! ###########"
+root_domain=$1
+echo "-----------------   Using ASSETFINDER      -----------------"
+cat $root_domain | assetfinder -subs-only | anew domains
+echo "-----------------   Finshed ASSETFINDER    -----------------"
+echo "-----------------   Using SUBFINDER    -----------------"
+subfinder -silent -dL $root_domain| anew domains
+echo "-----------------   Finshed SUBFINDER    -----------------"
+echo "-----------------   Using CRT_SH    -----------------"
+for i in $(cat $root_domain);do curl "https://crt.sh/?q=$i&output=json" | jq -r ".[].common_name" | sed 's/\*.//g' | anew domains;done 
+echo "-----------------   Finshed CRT_SH    -----------------"
+echo "-----------------   THE END  -----------------"
+cat domains | httpx | anew hosts
+cat domains | httprobe | anew hosts
